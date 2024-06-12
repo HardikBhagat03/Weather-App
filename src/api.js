5348b5929123c213df1adbd230a27da25272f126
Hardik Bhagat <113364012+HardikBhagat03@users.noreply.github.com>
2024-06-12 23:50:34 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+//const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions';
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '5c747cb1c7msha2930337a6f9386p10bc21jsn34661738b770',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "b08a1464229231eaf139220baea8ecfe";
